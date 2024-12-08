6506f9d796ae252a68d4fdcc4e8480ebd9ca4360
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-08 17:44:37 +0200

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1) Write a program that allows the user to enter a number then print it.
+
+            //string userInput=Console.ReadLine();
+
+            //if (int.TryParse(userInput, out int number))
+            //{
+            //    Console.WriteLine("Number is " + number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please enter a valid number.");
+            //}
+            #endregion
+
+
+        }
+    }
+}
